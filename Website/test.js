21e00aaa389712f8e4b795304896e17965e8d3fe
Kobe Dieryck <114076056+TheNeg0t1ator@users.noreply.github.com>
2023-05-14 01:29:51 +0200

    Json parse test functional
    Using XHR json parse.

--- Website/test.js
+++ Website/test.js
@@ -9,7 +9,8 @@ var i = 0;
             if (width >= 100) {
               clearInterval(id);
               i = 0;
-              show();
+              show('test');
+              Ajax_request();
             } else {
                 if(width == 1){hide('test');}
 
@@ -22,8 +23,30 @@ var i = 0;
 
       function hide(hide_id) {
         document.getElementById(hide_id).hidden = true
-        console.log('got out of the function');
     }
-     function show() {
-        document.getElementById('test').hidden = false
+     function show(hide_id) {
+        document.getElementById(hide_id).hidden = false
      }
+
+     
+     function Ajax_request(){
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "Data.json",true); // connectie met de server json file 
+        xhr.onload = function () { // data inlade
+          if (xhr.status == 200) {
+            console.log("okay");
+            var WeatherData = JSON.parse(xhr.responseText); //parse functie
+            renderHTML_data(WeatherData); // data render functie 
+          }
+          else
+          console.log("could not find file");
+        }  
+        xhr.send();    // request versturen
+       }
+
+       function renderHTML_data(WeatherData){
+        console.log(WeatherData[0].temp);
+        var elem = document.getElementById("myDataBar");
+        elem.style.width = WeatherData[0].temp*2 + "%";
+
+       }
